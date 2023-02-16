28381ceb8d6927f81f2c09dc60a901cbc7924fd1
Skalman <toonnes@hotmail.com>
2023-02-16 16:10:43 +0100

    Fixed download tabs and some cleanup

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+	"use strict";
+
+    //current year in footer
+    var currentYear = new Date().getFullYear();
+    $(".current-year").text("-" + currentYear);
+});
